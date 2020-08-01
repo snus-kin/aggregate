645758acc1ff210f1b2b0aebeea3e4a79c78ed80
Thomas Carroll <tcarroll@snufk.in>
2020-08-01 20:14:33 +0100

    now have a way of adding posts based on 'token' auth

--- src/aggregate.nim
+++ src/aggregate.nim
@@ -8,18 +8,65 @@ let db = open("posts.db", "", "", "")
 
 routes:
   get "/posts/list.json":
-    let posts = db.getAllRows(sql"SELECT * FROM posts")
+    ## GET a list of the posts, we should sort by timestamp
+    # unauthed
+    let posts = db.getAllRows(sql"SELECT * FROM posts ORDER BY time DESC")
     var jsonPosts: seq[JsonNode]
     for post in posts:
       var jsonPost = %* {
         "id": post[0],
         "name": post[1],
-        "description": post[2]
+        "description": post[2],
+        "time": post[3]
       }
       echo pretty jsonPost
       jsonPosts.add jsonPost
-    let jsonBody = %* jsonPosts
     
+    let jsonBody = %* jsonPosts
     resp jsonBody
+
+  get "/posts/latest.json":
+    ## GET the latest post
+    # unauthed
+    let post = db.getRow(sql"SELECT * FROM posts ORDER BY time DESC")
+    var jsonPost = %* {
+      "id": post[0],
+      "name": post[1],
+      "description": post[2],
+      "time": post[3]
+    }
+
+    resp jsonPost
+
+  post "/posts/create.json":
+    ## POST to create a new post
+    ## Authorization header must be set
+    ## example body:
+    ## ```json
+    ## {
+    ##  "type": "link",
+    ##  "title": "An Example Post",
+    ##  "link": "https://link.com",
+    ##  "description": "lorem ipsum"
+    ## }```
+    try: 
+      let payload = parseJson(request.body)
+      if request.headers.hasKey("Authorization"):
+        let token = request.headers["Authorization"]
+        let username = db.getValue(sql"SELECT username FROM users WHERE token == ?", token)
+        if username == "":
+          # No token with a username
+          resp Http403
+
+        echo pretty payload
+        db.exec(sql""" INSERT INTO posts (name, description) VALUES (?, ?)""", payload["title"], payload["description"])
+        # all is good
+        resp Http200
+      else:
+        # must be authed
+        resp Http400
+    except JsonParsingError, KeyError:
+      # if we can't parse it's a bad request
+      resp Http400
 
 db.close()
--- src/db.nim
+++ src/db.nim
@@ -1,15 +1,27 @@
 ## DB SETUP SCRIPT
+## temp dev env
 import db_sqlite
 let db = open("posts.db", "", "", "")
 
-db.exec(sql"""DROP TABLE IF EXISTS posts""")
-db.exec(sql"""CREATE TABLE posts (
-                 id   INTEGER PRIMARY KEY,
+# db.exec(sql"""DROP TABLE IF EXISTS posts""")
+# db.exec(sql"""DROP TABLE IF EXISTS users""")
+db.exec(sql"""CREATE TABLE IF NOT EXISTS posts (
+                 id   INTEGER PRIMARY KEY AUTOINCREMENT,
                  name VARCHAR(50) NOT NULL,
-                 description VARCHAR(120) NOT NULL
+                 description VARCHAR(120) NOT NULL,
+                 time DATETIME DEFAULT CURRENT_TIMESTAMP
               )""")
 
-db.exec(sql"INSERT INTO posts VALUES (1, 'test', 'testdesc')")
-db.exec(sql"INSERT INTO posts VALUES (2, 'test2', 'testdesc2')")
+db.exec(sql"""CREATE TABLE IF NOT EXISTS users (
+                 token VARCHAR(64) NOT NULL UNIQUE,
+                 username VARCHAR(50) NOT NULL UNIQUE
+              )""")
+
+
+# db.exec(sql"INSERT INTO posts (name, description) VALUES ('test', 'testdesc')")
+# db.exec(sql"INSERT INTO posts (name, description) VALUES ('test2', 'testdesc2')")
+# 
+# db.exec(sql"INSERT INTO users VALUES ('blah', 'testUSER1')")
+# db.exec(sql"INSERT INTO users VALUES ('blah1', 'testUSER2')")
 
 db.close()
