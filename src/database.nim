5dca4324cdbf1b00e6922f938c68fcf88504f287
Thomas Carroll <tcarroll@snufk.in>
2020-08-02 11:28:08 +0100

    Config and database modularised

---
+++ src/database.nim
@@ -0,0 +1,17 @@
+import db_sqlite
+proc makeTables*(db: DbConn): void =
+  db.exec(sql"""CREATE TABLE IF NOT EXISTS posts (
+                   id   INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT NOT NULL,
+                   title TEXT NOT NULL,
+                   link TEXT NOT NULL,
+                   description TEXT,
+                   time DATETIME DEFAULT CURRENT_TIMESTAMP
+                )""")
+  db.exec(sql"""CREATE TABLE IF NOT EXISTS users (
+                   token TEXT NOT NULL,
+                   username TEXT NOT NULL UNIQUE,
+                   salt TEXT NOT NULL
+                )""")
+# db.exec(sql"INSERT INTO posts (username, title, link, description) VALUES ('testUSER1', 'test', 'https://snufk.in', 'testdesc')")
+# db.exec(sql"INSERT INTO posts (username, title, link, description) VALUES ('testUSER2', 'test2', 'https://snufk.in/blog', 'testdesc2')")
